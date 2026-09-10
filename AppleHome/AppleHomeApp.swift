a4ae905a98beaf16393f9f9f2343683b580abbf9
Jitwisut Thobut <tanzajitwisut@gmail.com>
2026-09-11 00:57:04 +0700

    Initial Commit

---
+++ AppleHome/AppleHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleHomeApp.swift
+//  AppleHome
+//
+//  Created by Jitwisut Thobut on 11/9/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct AppleHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
